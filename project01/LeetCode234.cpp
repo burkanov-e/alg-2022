f4f8db8a0cf55a5bb585f22e2d1bc607f8b16ef8
burkanov-e <burkanov_e@auca.kg>
2022-03-28 15:19:33 +0200

    LeetCode234 Wrong Answer

---
+++ project01/LeetCode234.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+   public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+
+        vector<int> v;
+
+        while (slow != NULL) {
+            v.push_back(slow->val);
+            slow = slow->next;
+        }
+
+        while (head != NULL) {
+            int temp = v.back();
+
+            if (head->val != temp) {
+                return false;
+            }
+
+            head = head->next;
+        }
+        return true;
+    }
+};
