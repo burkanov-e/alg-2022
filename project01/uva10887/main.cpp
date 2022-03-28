43e0b42ea953dfd833ab913b8831630fc44a3f97
burkanov-e <burkanov_e@auca.kg>
2022-03-28 14:31:12 +0200

    uva10887 Accepted

--- project01/uva10887/main.cpp
+++ project01/uva10887/main.cpp
@@ -10,27 +10,38 @@ int main() {
 
     int test_case;
     cin >> test_case;
-    for (int i = 0; i < test_case; i++) {
+    string text;
+
+    for (int test = 0; test < test_case; test++) {
         int inp1, inp2;
         cin >> inp1 >> inp2;
 
-        unordered_set<string> set1;
-        unordered_set<string> set2;
+        vector<string> v1;
+        vector<string> v2;
 
-        while (inp1--) {
-            string text;
-            cin >> text;
-            set1.insert(text);
+        unordered_set<string> set;
+
+        getline(cin, text);
+
+        for (int i = 0; i < inp1; i++) {
+            getline(cin, text);
+            v1.push_back(text);
         }
 
-        while (inp2--) {
-            string text;
-            cin >> text;
-            set2.insert(text);
+        for (int i = 0; i < inp2; i++) {
+            getline(cin, text);
+            v2.push_back(text);
         }
 
-        int c = set1.size() * set2.size();
+        string temp;
 
-        cout << "Case " << i + 1 << ": " << c << endl;
+        for (int i = 0; i < inp1; i++) {
+            for (int j = 0; j < inp2; j++) {
+                temp = v1[i] + v2[j];
+                set.insert(temp);
+            }
+        }
+
+        cout << "Case " << test + 1 << ": " << set.size() << endl;
     }
 }
