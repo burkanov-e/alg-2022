5f4a303d0a0a3b6fd6e93d50302456178e87756e
burkanov-e <burkanov_e@auca.kg>
2022-05-11 13:47:42 +0200

    LeetCode897 Wrong Answer

---
+++ project02/LeetCode897.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    TreeNode* head = new TreeNode(0);
+    TreeNode* temp = head;
+
+    void rec(TreeNode* root) {
+        if (root == nullptr) {
+            return;
+        }
+        rec(root->left);
+        head->right = new TreeNode(root->val);
+        head = head->right;
+        rec(root->right);
+
+        return;
+    }
+    TreeNode* increasingBST(TreeNode* root) {
+        return temp->right;
+    }
+};
